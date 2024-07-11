f9ef5b200ee573adff152703e06fcd834a91a355
SRINJOY DAS <115316528+srinju@users.noreply.github.com>
2024-07-12 02:24:04 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+    <App /> //this part is basically shoves the react part into the index.html file
+  
 )
