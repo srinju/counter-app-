01d6db075e9fcaa85a0ca516dc44fdf23b66407a
SRINJOY DAS <115316528+srinju@users.noreply.github.com>
2024-07-12 02:23:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,34 @@
-//jsx files are combination of js and html , so you can see you can return a html inside a function 
+//jsx files are combination of js and xml , so you can see you can return a html inside a function
 
-import { useState } from 'react'
+import { useState } from 'react' //this is used to define the state , if we use useState then react will watch the state
 import './App.css'
 import React from 'react'
 
-function App() {
-  const [count, setCount] = React.useState(0)
+function App() { //this function defines our initial state and return/renders  a div that is the button component
+  //what the count and setcount is that setcount lets you set the count you call the count or do whatever you need to do in the count but call it inside setcount
+  const [count, setCount] = useState(0) // this is the initial state....react uses array destructuring . so count and setcount are the two things that the React.useState(0) function calls returns you . first value of the array you put on the count and the second value of the array you put on the setCount
 
   return ( //the thing that is getting rendered in our website is from this return part inside the function app if you type hi there then it will show just hi there instead of any buttons 
     <div> 
-        <Button count={count} setCount={setCount}></Button>      
+        <CustomButton count={count} setCount={setCount}></CustomButton> <br></br>
+        <CustomButton count={count + 1} setCount={setCount}></CustomButton> <br></br>
+        <CustomButton count={count + 10} setCount={setCount}></CustomButton> <br></br>
+        <CustomButton count={count + 100} setCount={setCount}></CustomButton>      
     </div>
   )
 }
 
-function Button(props){ //this func is same as the button component funciton we defined before coding react see the ppt of react notes
-  function onButtonClick() {
-    props.setCount(props.count + 1);
+//component >>
+//try to look at the code you will inderstand
+
+function CustomButton(props){
+
+  function onClickHandler() {
+    props.setCount(props.count+1);
   }
-  return <button onClick={onButtonClick}>Counter {props.count}</button>
+
+  return <button onClick={onClickHandler}>Counter {props.count}</button> 
 }
+
 
 export default App
